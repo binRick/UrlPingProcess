fe5a7cbc14cb5807b363ccd4573785ab62e4fa29
root <root@status.infinitumtech.com>
2014-12-11 18:22:35 -0500

    new files

---
+++ Config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    connection: {
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: 'as9dta97sdt8a9sdt',
+        database: 'observium'
+    },
+    Data: {
+        ProcessComplete: function(exitCode, Stdout, Stderr) {
+            return ' Process completed with code: ' + exitCode;
+            console.log('stdout log lines', Stdout);
+        },
+        stdoutLines: function(lines) {
+            console.log('stdout log lines', lines);
+            return 'new lines' + lines.length;
+        },
+        stderrLines: function(lines) {
+            console.log('stderr log lines', lines);
+            return 'new err lines' + lines.length;
+        },
+    },
+    urlMatch: '/:device_id',
+    queryBuilder: function(parameters) {
+        return 'delete from ports_vlans where device_id="' + parameters.device_id + '"';
+    },
+    spawnCommand: '/opt/observium/discovery.php',
+    spawnCommandArguments: function(parameters) {
+        return ['-h', parameters.device_id];
+    },
+};
---
+++ Server.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+var Config = require('./Config'),
+    express = require('express'),
+    app = new express(),
+    logger = require('morgan'),
+    mysql = require('mysql'),
+    connection = mysql.createConnection(Config.connection),
+    util = require('util'),
+    spawn = require('child_process').spawn;
+
+connection.connect();
+app.use(logger());
+
+app.get(Config.urlMatch, function(req, res) {
+    connection.query(Config.queryBuilder(req.params), function(err, rows, fields) {
+        if (err) throw err;
+        var OutLines = {
+            stdout: '',
+            stderr: ''
+        };
+        var Spawn = spawn(Config.spawnCommand, Config.spawnCommandArguments(req.params) || []);
+        Spawn.stdout.setEncoding('utf8');
+        Spawn.stdout.on('data', function(data) {
+            var str = data.toString(),
+                lines = str.split(/(\r?\n)/g);
+            OutLines.stdout.concat(lines);
+            res.write(Config.Data.stdoutLines(lines));
+        });
+        Spawn.stderr.on('data', function(data) {
+            var str = data.toString(),
+                lines = str.split(/(\r?\n)/g);
+            OutLines.stderr.concat(lines);
+            res.write(Config.Data.stderrLines(lines));
+        });
+        Spawn.on('exit', function(code) {
+            res.write(Config.Data.ProcessComplete(code, OutLines.stdout, OutLines.stderr));
+        });
+    });
+});
+app.listen(process.env.PORT || 49009, process.env.HOST || '127.0.0.1');
